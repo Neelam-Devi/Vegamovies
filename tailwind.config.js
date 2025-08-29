4cc5650b548dad54e5bd2e542c1d0390c770adff
Neelam-Devi <neelam8427021@gmail.com>
2025-08-29 13:03:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+// module.exports = {
+//     content: ["./src/**/*.{js,jsx,ts,tsx}"],
+//     theme: {
+//       extend: {
+      
+//       },
+//     },
+//     plugins: [],
+//   }
+  
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        gradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      animation: {
+        gradient: "gradient 8s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
